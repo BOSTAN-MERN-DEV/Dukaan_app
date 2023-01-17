1d9f2c7f80d984d480cef8fa9fc13b95a998fedb
Bostan-MERN_Dev <bostan.khan801@gmail.com>
2023-01-17 12:59:54 +0500

    backend architecture

---
+++ src/constants/constants.js
@@ -0,0 +1,30 @@
+const API_STATUS_CODES = {
+    SUCCESS: 200,
+    NOT_FOUND: 404,
+    AUTHORIZATION_FAILED: 401,
+    ERROR_CODE: 400,
+    INTERNAL_SERVER_ERROR: 500,
+    DUPLICATE_ENTRY: 11000,
+};
+const RESPONSE_MESSAGES = {
+    SUCCESS: 'Success',
+    AUTHORIZATION_FAILED: 'Authorization failed',
+    DUPLICATE_ENTRY: 'email already exist.',
+    MEAL_ADDED: 'meal added successfully',
+};
+
+const APP_ENVIRONMENTS = {
+    LOCAL: 'local',
+    DEV: 'dev',
+    STAGING: 'staging',
+    PRODUCTION: 'production',
+};
+
+const JWT_TOKEN = 'eyJhbGciOiJ$IUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwYzIzNTYyYjlkNTZkNjY1Yjk0NDkzYyIsInVzZXJOYW1lIjoibWFzaG9vZCBSYWZpIiwiaWF0IjoxNjIzMzQxNzU2fQ.vfJCCeMRdEhzkJEwgJybJ95zFm0eYuiYvUDpi2q3Of8';
+
+// export const ROLES = {
+//     USER: 1,
+//     ADMIN: 2,
+// };
+
+module.exports = { JWT_TOKEN, APP_ENVIRONMENTS, RESPONSE_MESSAGES, API_STATUS_CODES }
